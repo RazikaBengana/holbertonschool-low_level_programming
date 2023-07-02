c8675809bcafb61ebfbc9dfe48f40ad52b8f7f7a
RazikaBengana <razikabengana@gmail.com>
2023-07-02 12:59:51 +0200

    Header file

---
+++ recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar function */
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int findSquareRoot(int n, int estimatedSquareRoot);
+int _sqrt_recursion(int n);
+int check_divisibility(int num, int divisor);
+int is_prime_number(int n);
+
+
+#endif /* MAIN_H */
