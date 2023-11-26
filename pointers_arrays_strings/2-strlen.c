12184e447c5da91aa058cab26fc85134ab8d7630
RazikaBengana <razikabengana@gmail.com>
2023-11-26 18:05:48 +0100

    2. This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - program that returns the length of a string
+ *
+ * @s: the string to be measured
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
