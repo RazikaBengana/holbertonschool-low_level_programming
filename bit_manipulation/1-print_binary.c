aad4b9ab227ba55f5f57465666cff6b3a036a16b
RazikaBengana <razikabengana@gmail.com>
2023-11-26 23:57:40 +0100

    1. 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - program that prints the binary representation of a number
+ *
+ * @n: the decimal number to be converted and printed in binary
+ *
+ * Return: nothing (void)
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar('0' + (n & 1));
+}
