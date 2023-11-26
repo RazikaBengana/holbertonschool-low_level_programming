179a1e6e5f6c2ed39d1063807c500b9237552048
RazikaBengana <razikabengana@gmail.com>
2023-11-26 17:27:43 +0100

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - program that adds two integers and returns the result
+ *
+ * @m: the 1st integer
+ * @n: the 2nd integer
+ *
+ * Return: the result of the sum
+ */
+
+int add(int m, int n)
+{
+	return (m + n);
+}
