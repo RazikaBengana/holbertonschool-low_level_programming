e66f3181a2cc473e6cf7d27b63a4d24c1e160506
RazikaBengana <razikabengana@gmail.com>
2023-11-26 23:43:21 +0100

    _putchar function

---
+++ more_singly_linked_lists/_putchar.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * _putchar - program that writes a character to the standard output (stdout)
+ *
+ * @c: the character to print
+ *
+ * Return: on success, returns the number of bytes written (always 1);
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
