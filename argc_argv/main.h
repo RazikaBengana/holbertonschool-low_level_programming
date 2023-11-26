e894528d1472605b1b4105813e116b80f3a81579
RazikaBengana <razikabengana@gmail.com>
2023-11-26 21:29:29 +0100

    Header file

---
+++ argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/* _putchar function (if needed) */
+int _putchar(char c);
+
+
+#endif /* MAIN_H */
